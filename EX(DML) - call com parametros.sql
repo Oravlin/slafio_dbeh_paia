c8e68a5dd8e453484a0aeabf39935a864b36450a
Alvaro <alvaroandradesantos2009@gmail.com>
2025-11-05 12:54:40 -0300

    slafiot 3.0 (fazendo até o 15)

--- EX(DML) - call com parametros.sql
+++ EX(DML) - call com parametros.sql
@@ -217,3 +217,121 @@ call inserir_clientepj(7, 'Caloteando', 69, null, 12345053, 12345678912346, 9876
 call inserir_clientepj(8, 'Semgrana', 189, null, 12345060, 12345678912347, 98765432100);
 call inserir_clientepj(9, 'Cemreais', 5024, 'Sala 23', 12345060, 12345678912348, 98765432101);
 call inserir_clientepj(10, 'Durango', 1254, null, 12345060, 12345678912349, 98765432102);
+
+-- EX9 - Compras
+
+delimiter $$
+
+create procedure spinserir_compras(
+pNotaFiscal int,
+pDataCompra varchar(50),
+pValorTotal decimal(8,2),
+pQtdTotal int,
+pCodigo decimal(14,0),
+vFornecedor varchar(200),
+pValorItem decimal(8,2),
+pQtd int)
+begin
+if exists (select 1 from tbfornecedor where Nome = vFornecedor) and exists (select 1 from tbproduto where CodigoBarras = pCodigo) then
+        insert into tbcompra (NotaFiscal, DataCompra, ValorTotal, QtdTotal)
+        values (pNotaFiscal, STR_TO_DATE(pDataCompra, '%d/%m/%Y'), pValorTotal, pQtdTotal);
+
+        insert into tbitemcompra (ValorItem, CodigoBarras, NotaFiscal, Qtd)
+        values (pValorItem, pCodigo, pNotaFiscal, pQtd);
+else 
+signal sqlstate '45000' set message_text = 'erro, não foi possível encontrar fornecedor ou já há uma duplicação de pk';
+end if;
+end$$
+delimiter;
+
+call spinserir_compras(8459, '01/05/2018', 21944.00, 700, 12345678910111, 'Amoroso e Doce', 22.22, 200);
+call spinserir_compras(2482, '22/04/2020', 7290.00, 180, 12345678910112, 'Revenda Chico Loco', 40.50, 180);
+call spinserir_compras(21563, '12/07/2020', 900.00, 300, 12345678910113, 'Marcelo Dedal', 3.00, 300);
+call spinserir_compras(8449, '01/05/2018', 21944.00, 700, 12345678910114, 'Amoroso e Doce', 35.00, 500);
+call spinserir_compras(156354, '23/11/2021', 18900.00, 350, 12345678910115, 'Revenda Chico Loco', 54.00, 350);
+
+-- EX10 - registro vendas
+
+delimiter $$
+create procedure spinserir_registrovenda(
+pNumeroVenda int,
+pCliente varchar(200),
+pCodigoBarras decimal(14,0),
+pQtd int,
+pTotalVenda decimal(8,2))
+begin
+declare vIdCli int;
+declare vValorItem decimal(8,2);
+if exists (select 1 from tbcliente where NomeCli = pCliente) and exists (select 1 from tbproduto where CodigoBarras = pCodigoBarras) then
+	select IdCli into vIdCli from tbcliente where NomeCli = pCliente;
+    select Valor into vValorItem from tbproduto where CodigoBarras = pCodigoBarras;
+    
+     insert into tbvenda(NumeroVenda, DataVenda, TotalVenda, IdCli)
+    values(pNumeroVenda,current_date(), pTotalVenda, vIdCli);
+
+	insert into tbitemvenda(NumeroVenda, CodigoBarras, ValorItem, Qtd)
+    values(pNumeroVenda, pCodigoBarras, vValorItem, pQtd);
+    
+   
+end if; 
+end$$
+call spinserir_registrovenda(1,'Pimpão', 12345678910111, 1, 54.61);
+call spinserir_registrovenda(2,'Lança Perfume', 12345678910112, 2, 200.90);
+call spinserir_registrovenda(3,'Pimpão', 12345678910113, 1, 44.00);
+
+-- EX11
+delimiter $$
+create procedure spinserir_notafiscal(
+pNF int,
+pCliente varchar(200))
+begin
+	declare vTotalNota decimal(8,2);
+	declare vIdCli int;
+	if exists (select 1 from tbcliente where NomeCli = pCliente) then
+		select IdCli into vIdCli from tbcliente where NomeCli = pCliente;
+        select sum(TotalVenda) into vTotalNota from tbvenda where IdCli = vIdCli;
+        
+        if vTotalNota > 0 then
+			insert into tbnota_fiscal(NF, DataEmissao, TotalNota)
+			values(pNF, current_date(), vTotalNota);
+		end if;
+	end if;
+end$$
+
+call spinserir_notafiscal(359, 'Pimpão');
+call spinserir_notafiscal(360, 'Lança Perfume');
+
+-- ex12
+call inserir_produto(12345678910130, 'Camiseta de Poliéster', 33.61, 100);
+call inserir_produto(12345678910131, 'Blusa Frio Moletom', 200.00, 100);
+call inserir_produto(12345678910132, 'Vestido Decote Redondo', 144.00, 50);
+
+-- Ex13
+delimiter $$
+create procedure spapagar_produtos(
+pCodigoBarras decimal(14,0))
+begin
+delete from tbproduto where CodigoBarras = pCodigoBarras;
+end$$
+call spapagar_produtos(12345678910116);
+call spapagar_produtos(12345678910117);
+
+-- ex14
+delimiter $$
+create procedure spupdate_produtos(
+pCodigoBarras decimal(14,0),
+pNome varchar(200),
+pValorUnitario decimal(8,2))
+begin
+update tbproduto set Nome = pNome, Valor = pValorUnitario where CodigoBarras = pCodigoBarras;
+end$$
+call spupdate_produtos(12345678910111, 'Rei de Papel Mache', 64.50);
+call spupdate_produtos(12345678910111, 'Rei de Papel Mache', 64.50);
+
+-- Ex15
+delimiter $$
+create procedure spmostrar_produtos()
+begin
+select * from tbproduto;
+end$$
+call spmostrar_produtos();
